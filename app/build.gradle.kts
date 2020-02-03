a55ef57ecf86cdecdc319d70d14a46fbfb0305e2
ruX <post4ruX@gmail.com>
2020-02-03 17:10:39 +0000

    Moved project to kotlin-based gradle script

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    id("androidx.navigation.safeargs")
+}
+
+android {
+    compileSdkVersion(29)
+
+    defaultConfig {
+        applicationId = "vc.rux.klinent4todobackend"
+        minSdkVersion(19)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+// To inline the bytecode built with JVM target 1.8 into
+// bytecode that is being built with JVM target 1.6. (e.g. navArgs)
+
+
+
+    compileOptions {
+        setSourceCompatibility(JavaVersion.VERSION_1_7)
+        setTargetCompatibility(JavaVersion.VERSION_1_7)
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.core:core-ktx:1.1.0")
+    implementation("com.google.android.material:material:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.0.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.0.0")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+}
